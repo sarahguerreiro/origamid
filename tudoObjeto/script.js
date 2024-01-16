b9afaf3febb0897d4465c940d384f82edab76f26
Sarah <sdmguerre@gmail.com>
2024-01-16 16:19:48 -0300

    exercicios tudoObjeto

--- tudoObjeto/script.js
+++ tudoObjeto/script.js
@@ -1,2 +1,27 @@
 var nome = 'siclano'
 
+var nomeMinusculo = nome.toLocaleLowerCase()
+
+console.log(nomeMinusculo)
+
+// o querySelector te traz um elemento a partir da sua id (#) ou da sua class(.), entao sempre q vc quiser um elemento html via js, vc pode usar o querySelctor e passar como argumento .nomeDaClasse ou #nomeDaId:
+
+let btn = document.querySelector('.btn')
+
+// nomeie 3 propriedades ou metodos de strings
+
+bold
+toUpperCase
+length
+
+
+// nomeie 5 propriedades/elementos dom 
+
+.addEventListener()
+.appendChild()
+.id()
+.innerHtml()
+.outerHtml()
+
+// busque na internet um objeto(método) capaz de interagir com o clipboard - que é a parte do pc que lida com o CTRL + C e CTRL + V
+
