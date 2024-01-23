d7f5554e16623569497b2a852ce4cfe66fe43d2a
Sarah <sdmguerre@gmail.com>
2024-01-23 18:56:04 -0300

    dupliquei a pasta de animais fantasticos pra testar na copia

---
+++ dom/animaisFantasticos copy/script.js
@@ -0,0 +1,17 @@
+
+const animais = document.getElementById('animais')
+console.log(animais.innerText)
+
+const gridSection = document.getElementsByClassName('grid-section')
+console.log(gridSection[1])
+
+const primeiraLi = document.querySelector('li')
+console.log(primeiraLi)
+
+const primeiraUl = document.querySelector('ul')
+console.log(primeiraUl)
+
+
+
+
+
