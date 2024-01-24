2a07e00935015a8e61d48a8740b45145651e726e
Sarah <sdmguerre@gmail.com>
2024-01-24 18:45:11 -0300

    cria a pasta forEach-e-arrowFunction e copia pra dentro dela o arquivo que estamos trabalhando

---
+++ dom/forEach-e-arrowFunction/script.js
@@ -0,0 +1,25 @@
+
+const animais = document.getElementById('animais')
+console.log(animais.innerText)
+
+const gridSection = document.getElementsByClassName('grid-section')
+console.log(gridSection[1])
+
+const primeiraLi = document.querySelector('li')
+console.log(primeiraLi)
+
+const primeiraUl = document.querySelector('ul')
+console.log(primeiraUl)
+
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno)
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg)
+
+const gridSectionHtml = document.getElementsByClassName('grid-section')
+const gridSectionNode = document.getElementsByClassName('grid-section')
+
+console.log(gridSectionHtml)
+console.log(gridSectionNode)
+
