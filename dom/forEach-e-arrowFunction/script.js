cbf7eddb12b8a7635ae5fb71e0075a4bb41af0f6
Sarah <sdmguerre@gmail.com>
2024-01-27 11:33:36 -0300

    inicia arrowFunction e forEach

--- dom/forEach-e-arrowFunction/script.js
+++ dom/forEach-e-arrowFunction/script.js
@@ -1,25 +1,2 @@
-
-const animais = document.getElementById('animais')
-console.log(animais.innerText)
-
-const gridSection = document.getElementsByClassName('grid-section')
-console.log(gridSection[1])
-
-const primeiraLi = document.querySelector('li')
-console.log(primeiraLi)
-
-const primeiraUl = document.querySelector('ul')
-console.log(primeiraUl)
-
-const linkInterno = document.querySelector('[href^="#"]');
-console.log(linkInterno)
-
-const animaisImg = document.querySelectorAll('.animais img');
-console.log(animaisImg)
-
-const gridSectionHtml = document.getElementsByClassName('grid-section')
-const gridSectionNode = document.getElementsByClassName('grid-section')
-
-console.log(gridSectionHtml)
-console.log(gridSectionNode)
-
+ const imgs = document.querySelectorAll('img')
+    console.log(imgs)
